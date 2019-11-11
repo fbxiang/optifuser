fc48acc1a8fcc7641bee0f2d7cf82f39bf17a946
Fanbo Xiang <xiangfanbo@gmail.com>
2019-11-11 13:19:19 -0800

    fix debug texture name

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -61,7 +61,7 @@ void Renderer::initTextures() {
   glTexImage2D(GL_TEXTURE_2D, 0, GL_R32I, m_width, m_height, 0, GL_RED_INTEGER, GL_INT, NULL);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  LABEL_TEXTURE(segtex[0], "segmentation tex 2");
+  LABEL_TEXTURE(segtex[1], "segmentation tex 2");
 
   glBindTexture(GL_TEXTURE_2D, segtex[2]);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, m_width, m_height, 0, GL_RGBA, GL_FLOAT, NULL);
