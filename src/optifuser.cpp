ef42c8c8483e9d005e848c8d9742483314894f38
Fanbo Xiang <xiangfanbo@gmail.com>
2021-03-04 20:06:45 -0800

    imgui version

--- src/optifuser.cpp
+++ src/optifuser.cpp
@@ -1,7 +1,7 @@
 #include "optifuser.h"
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 #include <spdlog/spdlog.h>
 #include "spdlog/sinks/stdout_color_sinks.h"
 
