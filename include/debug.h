901ad789114d427bcc2d07f9e25cfd712444eab6
Jet Zhang <jetgabr@gmail.com>
2021-09-03 22:43:27 -0700

    [FIX] remove compilation complaints

--- include/debug.h
+++ include/debug.h
@@ -8,18 +8,19 @@
   }                                                                   \
   printf("\n");
 
-// #define LABEL_TEXTURE(id, label)                                               \
-//   {                                                                            \
-//     std::string _l = label;                                                    \
-//     glObjectLabel(GL_TEXTURE, id, _l.length(), _l.c_str());                    \
-//   }
+/*
+ #define LABEL_TEXTURE(id, label)                                               \
+   {                                                                            \
+     std::string _l = label;                                                    \
+     glObjectLabel(GL_TEXTURE, id, _l.length(), _l.c_str());                    \
+   }
 
-// #define LABEL_FRAMEBUFFER(id, label)                                           \
-//   {                                                                            \
-//     std::string _l = label;                                                    \
-//     glObjectLabel(GL_FRAMEBUFFER, id, _l.length(), _l.c_str());                \
-//   }
-
+ #define LABEL_FRAMEBUFFER(id, label)                                           \
+   {                                                                            \
+     std::string _l = label;                                                    \
+     glObjectLabel(GL_FRAMEBUFFER, id, _l.length(), _l.c_str());                \
+   }
+*/
 
 #define LABEL_TEXTURE(id, label) {}
 #define LABEL_FRAMEBUFFER(id, label) {}
